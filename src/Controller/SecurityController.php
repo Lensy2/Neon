c9cf422a8a6b59af35cbbce547e3b89d4f8df22e
lensy <yurane21@hotmail.com>
2018-04-11 10:08:27 -0500

    Se implementa el acceso a la aplicación.

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Forms\Type\Form;
+use Symfony\Component\Security\Core\SecurityContext;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/acceso", name="acceso")
+     */
+    public function accesoAction(Request $request){
+
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction(){
+        throw new \RuntimeException('No puede ser llamada directamente
+        ');
+    }
+
+
+    /**
+     * @Route("/acceso2", name="acceso2")
+     */
+    public function acceso2Action(Request $request){
+
+
+        return $this->render('Loginlogin2.html.twig');
+
+    }
+
+}
