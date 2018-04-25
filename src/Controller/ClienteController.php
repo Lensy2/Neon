1991e9fd0af5802e10b622a307e26900d578b21b
lensy <yurane21@hotmail.com>
2018-04-25 16:52:12 -0500

    Creación de clientes.

--- src/Controller/ClienteController.php
+++ src/Controller/ClienteController.php
@@ -8,11 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ClienteController extends Controller
 {
 
-    var $strDqlLista = "";
-    var $strCodigo = "";
-    var $strNombre = "";
-    var $strIdentificacion = "";
-
 }
 
 
